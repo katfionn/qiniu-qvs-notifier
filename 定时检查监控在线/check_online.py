882dc73c520d7447a458b67ff0cc1cd7f28518ae
Katfionn <136874302+katfionn@users.noreply.github.com>
2026-09-02 18:40:48 +0800

    change few details

--- 定时检查监控在线/check_online.py
+++ 定时检查监控在线/check_online.py
@@ -72,8 +72,8 @@ def listNamespacesInfo(access_key, secret_key, namespaceId, gbId):
 
 if __name__ == '__main__':
     #  AKSK
-    access_key = '_cuhfevwcyWnDoIo6fRPXiI_Bs_vpCqv7EiGT1AH'
-    secret_key = 'LwXvSgvUbcP0A52iQ1BqaKTCQbds5IKIAqtfosVZ'
+    access_key = '{{access_key}}'
+    secret_key = '{{secret_key}}'
 
     with open('devices.txt', 'r') as file:
         for line in file:
